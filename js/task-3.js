cb0c34a809dbf71e99d2f0670d8d374151fc9357
Oleksandr Osadchyi <ooe@ukr.net>
2025-06-10 16:15:33 +0300

    task complite

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,5 @@
-const sortByDescendingFriendCount = (users) => {
+const sortByDescendingFriendCount = (users) =>
   users.toSorted((a, b) => b.friends.length - a.friends.length);
-};
 
 console.log(
   sortByDescendingFriendCount([
